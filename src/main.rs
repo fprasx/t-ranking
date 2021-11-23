7db3fbdb0bdc996d4fd9bc8d799316f34675045c
Felix Prasanna <felix725@gmail.com>
2021-11-22 19:47:45 -0500

    Fixed precommit by chaning grep -e to grep -E and change \e's to \033's.

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,10 @@ use std::env;
 
 #[get("/")]
 fn index() -> String {
-	// Gets USER env variable 
-	let name = env::var("USER").unwrap_or(String::from("Couldn't find hostname"));
-	return  format!("Hello, {}", name)
+	// Gets USER env variable
+	let name = env::var("USER",).unwrap_or_else(|_| String::from("Couldn't find hostname",),);
+	return format!("Hello, {}", name);
 }
 
 #[launch]
-fn rocket() -> _ {
-	rocket::build().mount("/", routes![index])
-}
+fn rocket() -> _ { rocket::build().mount("/", routes![index],) }
