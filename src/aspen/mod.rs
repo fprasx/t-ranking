6194d4645b5008e6cec073c0473d644f9f649c78
psvenk <45520974+psvenk@users.noreply.github.com>
2021-11-26 12:27:42 -0500

    Attempt to replicate Aspine behavior
    Code is still not fully working, and formatting needs to be changed.

--- src/aspen/mod.rs
+++ src/aspen/mod.rs
@@ -5,60 +5,75 @@ use reqwest;
 
 #[derive(Clone)]
 pub struct AspenInfo {
-	pub session_id:   String,
-	pub apache_token: String,
+    pub session_id: String,
+    pub apache_token: String,
 }
 
 impl AspenInfo {
-	fn new(session_id: String, apache_token: String) -> AspenInfo {
-		AspenInfo {
-			session_id,
-			apache_token,
-		}
-	}
+    fn new(session_id: String, apache_token: String) -> AspenInfo {
+        AspenInfo {
+            session_id,
+            apache_token,
+        }
+    }
 }
 
 #[derive(Debug)]
 pub enum AspenError {
-	NoSession,
+    NoSession,
 }
 
 // Placeholder implementation
 impl fmt::Display for AspenError {
-	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		// As more error types are created this should basically become a case statement
-		match self {
-			AspenError::NoSession => write!(f, "NoSession Error, Invalid Response Returned"),
-		}
-	}
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // As more error types are created this should basically become a case statement
+        match self {
+            AspenError::NoSession => write!(f, "NoSession Error, Invalid Response Returned"),
+        }
+    }
 }
 
 // Placeholder implementation
 impl Error for AspenError {
-	fn description(&self) -> &str { "AspenError" }
+    fn description(&self) -> &str {
+        "AspenError"
+    }
 }
 
 // Request a session id from aspen for later use
 pub async fn get_session() -> Result<AspenInfo, Box<dyn Error + Send + Sync>> {
-	// Make request to aspen using reqwest convenience function
-	let res = reqwest::get("https://aspen.cpsd.us/aspen/logon.do")
-		.await?
-		.text()
-		.await?;
-	// Regex for finding session id in res (regex from https://github.com/Aspine/aspine/blob/master/src/scrape.ts:762)
-	let session_re = Regex::new("sessionId='(.+)';").unwrap();
-	// Regex for finding apache token in res (regex from https://github.com/Aspine/aspine/blob/master/src/scrape.ts:766)
-	let apache_token_re = Regex::new("name=\"org.apache.struts.taglib.html.TOKEN\" value=\"(.+)\"").unwrap();
-	// Look at regex documentation for match groups
-	let session = session_re.find(&res[..]); // EX: sessionId='2llmtAkaAwPnICzAVc_2qeK2RhRzpcVhdB4vhGbB';
-	let token = apache_token_re.find(&res[..]); // EX: name="org.apache.struts.taglib.html.TOKEN"
-											// value="843ad705c44d2f6cadf8b454db87fc39" If neither a session_id or
-											// apache_token is found, return an AspenError
-	if session.is_none() || token.is_none() {
-		return Err(Box::new(AspenError::NoSession));
-	}
-	// Unwrapping regex match and taking substrings to get just values
-	let session_ret = session.unwrap().as_str().chars().skip(11).take(40).collect::<String>();
-	let token_ret = token.unwrap().as_str().chars().skip(50).take(32).collect::<String>();
-	Ok(AspenInfo::new(session_ret, token_ret))
+    // Make request to aspen using reqwest convenience function
+    let res = reqwest::get("https://aspen.cpsd.us/aspen/logon.do")
+        .await?
+        .text()
+        .await?;
+    // Regex for finding session id in res (regex from https://github.com/Aspine/aspine/blob/master/src/scrape.ts:762)
+    let session_re = Regex::new("sessionId='(.+)';").unwrap();
+    // Regex for finding apache token in res (regex from https://github.com/Aspine/aspine/blob/master/src/scrape.ts:766)
+    let apache_token_re =
+        Regex::new("name=\"org.apache.struts.taglib.html.TOKEN\" value=\"(.+)\"").unwrap();
+    // Look at regex documentation for match groups
+    let session = session_re.find(&res[..]); // EX: sessionId='2llmtAkaAwPnICzAVc_2qeK2RhRzpcVhdB4vhGbB';
+    let token = apache_token_re.find(&res[..]); // EX: name="org.apache.struts.taglib.html.TOKEN"
+                                                // value="843ad705c44d2f6cadf8b454db87fc39" If neither a session_id or
+                                                // apache_token is found, return an AspenError
+    if session.is_none() || token.is_none() {
+        return Err(Box::new(AspenError::NoSession));
+    }
+    // Unwrapping regex match and taking substrings to get just values
+    let session_ret = session
+        .unwrap()
+        .as_str()
+        .chars()
+        .skip(11)
+        .take(40)
+        .collect::<String>();
+    let token_ret = token
+        .unwrap()
+        .as_str()
+        .chars()
+        .skip(50)
+        .take(32)
+        .collect::<String>();
+    Ok(AspenInfo::new(session_ret, token_ret))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,116 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 extern crate reqwest;
+use reqwest::{header, redirect};
 use std::env;
 use std::error::Error;
 
-use ranking::aspen;
+use ranking::aspen::{self, AspenInfo};
+
 // Right now all this does is retrieve a session id from aspen for later use
 #[get("/")]
 async fn index() -> String {
-	let session = aspen::get_session().await;
-	if let Ok(info) = session {
-		println!("Session_id: {}", info.session_id);
-		println!("Apache_token: {}", info.apache_token);
-		let login = login(&info).await;
-		match login {
-			Ok(l) => {
-				println!("Are we logged in? {}", !l.contains("Invalid login.")); // If the string is not found, then loggedin will be false, so negate it ;
-			}
-			Err(e) => println!("Error logging in: {}", e),
-		}
+    if let Ok(info) = aspen::get_session().await {
+        println!("Session_id: {}", info.session_id);
+        println!("Apache_token: {}", info.apache_token);
+        match login(&info).await {
+            Ok(l) => {
+                println!("Are we logged in? {}", !l.contains("Invalid login."));
+                // If the string is not found, then loggedin will be false, so negate it ;
+            }
+            Err(e) => println!("Error logging in: {}", e),
+        }
 
-		let student = get_student_info(&info).await;
-		match student {
-			Ok(student) => student,
-			Err(e) => format!("Errror: {:#?}", e),
-		}
-	}
-	else {
-		println!("Error Returned");
-		"Error Logging in".to_string()
-	}
+        match get_student_info(&info).await {
+            Ok(student) => student,
+            Err(e) => format!("Error: {:#?}", e),
+        }
+    } else {
+        println!("Error Returned");
+        "Error Logging in".to_string()
+    }
 }
 
-async fn login(credentials: &aspen::AspenInfo) -> Result<String, Box<dyn Error + Send + Sync>> {
-	let username = env::var("ASPEN_USERNAME").unwrap_or_else(|_| String::from("test_username"));
-	let password = env::var("ASPEN_PASSWORD").unwrap_or_else(|_| String::from("test_password"));
+async fn login(credentials: &AspenInfo) -> Result<String, Box<dyn Error + Send + Sync>> {
+    let username = env::var("ASPEN_USERNAME").unwrap();
+    let password = env::var("ASPEN_PASSWORD").unwrap();
 
-	let client = reqwest::Client::builder().build()?;
-	let res = client
-		.post("https://aspen.cpsd.us/aspen/logon.do")
-		.header("Cookie", format!("JSESSIONID={}", credentials.session_id))
-		// TODO: find some way of more efficiently generating request body!!!
-		.query(&["org.apache.struts.taglib.html.TOKEN", credentials.apache_token.as_str()])
-		.query(&["userEvent", "930"])
-		.query(&["deploymentId", "x2sis"])
-		.query(&["username", username.as_str()])
-		.query(&["password", password.as_str()])
-		// .body(format!(
-		// 	"org.apache.struts.taglib.html.TOKEN={}&userEvent=930&deploymentId=x2sis&username={}&password={}",
-		// 	credentials.apache_token, username, password
-		// ))
-		.send()
-		.await?
-		.text()
-		.await?;
-	Ok(res)
+    let client = reqwest::Client::builder()
+        .redirect(redirect::Policy::none())
+        .build()?;
+
+    let params = [
+        (
+            "org.apache.struts.taglib.html.TOKEN",
+            credentials.apache_token.clone(),
+        ),
+        ("userEvent", "930".to_string()),
+        ("deploymentId", "x2sis".to_string()),
+        ("username", username.clone()),
+        ("password", password.clone()),
+    ];
+
+    let res = client
+        // .post("https://httpbin.org/post")
+        .post("https://aspen.cpsd.us/aspen/logon.do")
+        .header(
+            header::COOKIE,
+            format!("JSESSIONID={}", credentials.session_id.clone()),
+        )
+        .header(
+            header::USER_AGENT,
+            "node-fetch/1.0 (+https://github.com/bitinn/node-fetch)",
+        )
+        .header(header::ACCEPT_ENCODING, "gzip,deflate")
+        .form(&params)
+        .header(
+            header::CONTENT_TYPE,
+            "application/x-www-form-urlencoded;charset=UTF-8",
+        )
+        .send()
+        .await?
+        .text()
+        .await?;
+    println!("{}", res);
+
+    /*
+    let res = client
+        .post("https://aspen.cpsd.us/aspen/logon.do")
+        .header("Cookie", format!("JSESSIONID={}", credentials.session_id))
+        // TODO: find some way of more efficiently generating request body!!!
+        // .query(&["org.apache.struts.taglib.html.TOKEN", credentials.apache_token.as_str()])
+        // .query(&["userEvent", "930"])
+        // .query(&["deploymentId", "x2sis"])
+        // .query(&["username", username.as_str()])
+        // .query(&["password", password.as_str()])
+        .body(format!(
+            "org.apache.struts.taglib.html.TOKEN={}&userEvent=930&deploymentId=x2sis&username={}&password={}",
+            credentials.apache_token, username, password
+        ))
+        .send()
+        .await?
+        .text()
+        .await?;
+    */
+    Ok("".to_string())
+    // Ok(res)
 }
 
-async fn get_student_info(credentials: &aspen::AspenInfo) -> Result<String, Box<dyn Error + Send + Sync>> {
-	let client = reqwest::Client::builder().build()?;
-	let res = client
-		.post("https://aspen.cpsd.us/aspen/rest/users/students")
-		.header("Cookie", format!("JSESSIONID={}", credentials.session_id))
-		.send()
-		.await?
-		.text()
-		.await?;
-	Ok(res)
+async fn get_student_info(
+    credentials: &aspen::AspenInfo,
+) -> Result<String, Box<dyn Error + Send + Sync>> {
+    let client = reqwest::Client::builder().build()?;
+    let res = client
+        .post("https://aspen.cpsd.us/aspen/rest/users/students")
+        .header("Cookie", format!("JSESSIONID={}", credentials.session_id))
+        .send()
+        .await?
+        .text()
+        .await?;
+    Ok(res)
 }
 
 #[launch]
-async fn rocket() -> _ { rocket::build().mount("/", routes![index]) }
+async fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
+}
