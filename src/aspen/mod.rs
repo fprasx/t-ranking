df1e8663c2712de6b6c858b163796a752ad17757
fprasx <felix725@gmail.com>
2021-11-26 13:54:09 -0500

    Rewriting get_session for better error handling
    Implemented regex match groups to avoid hard-coding in substring indices
    Improved error-handling of Option types for better readability

--- src/aspen/mod.rs
+++ src/aspen/mod.rs
@@ -47,33 +47,30 @@ pub async fn get_session() -> Result<AspenInfo, Box<dyn Error + Send + Sync>> {
         .await?
         .text()
         .await?;
-    // Regex for finding session id in res (regex from https://github.com/Aspine/aspine/blob/master/src/scrape.ts:762)
+    // Regex's from https://github.com/Aspine/aspine/blob/master/src/scrape.ts:762)
     let session_re = Regex::new("sessionId='(.+)';").unwrap();
-    // Regex for finding apache token in res (regex from https://github.com/Aspine/aspine/blob/master/src/scrape.ts:766)
     let apache_token_re =
         Regex::new("name=\"org.apache.struts.taglib.html.TOKEN\" value=\"(.+)\"").unwrap();
-    // Look at regex documentation for match groups
-    let session = session_re.find(&res[..]); // EX: sessionId='2llmtAkaAwPnICzAVc_2qeK2RhRzpcVhdB4vhGbB';
-    let token = apache_token_re.find(&res[..]); // EX: name="org.apache.struts.taglib.html.TOKEN"
-                                                // value="843ad705c44d2f6cadf8b454db87fc39" If neither a session_id or
-                                                // apache_token is found, return an AspenError
+    // EX: sessionId='2llmtAkaAwPnICzAVc_2qeK2RhRzpcVhdB4vhGbB';
+    let session = session_re.captures(res.as_str());
+    // EX: name="org.apache.struts.taglib.html.TOKEN" value="843ad705c44d2f6cadf8b454db87fc39"
+    let token = apache_token_re.captures(res.as_str());
+    // If one either the session_id or the apache token isn't found, return an error
     if session.is_none() || token.is_none() {
         return Err(Box::new(AspenError::NoSession));
     }
-    // Unwrapping regex match and taking substrings to get just values
-    let session_ret = session
-        .unwrap()
-        .as_str()
-        .chars()
-        .skip(11)
-        .take(40)
-        .collect::<String>();
-    let token_ret = token
-        .unwrap()
-        .as_str()
-        .chars()
-        .skip(50)
-        .take(32)
-        .collect::<String>();
-    Ok(AspenInfo::new(session_ret, token_ret))
+    // Check that both regex's mactched
+    if let (Some(s_inner), Some(t_inner)) = (session, token) {
+        // Check that the groups were found
+        if let (Some(s), Some(t)) = (s_inner.get(1), t_inner.get(1)) {
+            Ok(AspenInfo::new(
+                s.as_str().to_string(),
+                t.as_str().to_string(),
+            ))
+        } else {
+            Err(Box::new(AspenError::NoSession))
+        }
+    } else {
+        Err(Box::new(AspenError::NoSession))
+    }
 }
